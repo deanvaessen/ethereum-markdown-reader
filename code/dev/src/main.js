dda375e2408cec881bd8cb3eaed4fd7454103182
Dean Vaessen <hello@deanvaessen.com>
2017-01-12 23:04:25 +0100

    0.0.1 - Markdown test

--- code/dev/src/main.js
+++ code/dev/src/main.js
@@ -1,4 +1,44 @@
+/**
+ * Dependencies
+ */
 import 'src/style.scss';
+import marked from 'src/helpers/marked.js';
 
-console.log('this is a test');
+/**
+ * Variables
+ */
+const baseUrl = '/~nagydani/md/';
+
+/**
+ * Code to run on change of location hash
+ */
+const fragmentChange = function() {
+
+	if (window.location.hash) {
+		const fragment = window.location.hash.substring(1);
+
+		// Change page title to level 1 heading
+		const renderer = new marked.Renderer();
+
+		renderer.heading = function(text, level) {
+			if (level === 1) {
+				document.title = text;
+			}
+			return '<h' + level + '>' + text + '</h' + level + '>\n';
+		};
+
+		// render document to container
+		document.getElementById('content').innerHTML =
+		marked('# Marked in browser\n\nRendered by **marked**.', { renderer : renderer });
+		console.log(baseUrl + fragment);
+	} else {
+		// leave document empty
+	}
+};
+
+// Set the fragmentChange function to run each time the window on hash event fires
+window.onhashchange = fragmentChange;
+
+// Run the function
+fragmentChange();
 
